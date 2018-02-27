e7045df4e7ac58922694152ab74adf24d7509e5d
heminhua <15218482830@163.com>
2018-02-27 21:49:25 +0800

    集成element-ui

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <p>{{val}}</p>
-        <!-- element-ui -->
+        <!--测试 element-ui集成element-ui -->
+        <el-progress type="circle" :percentage="100" status="success"></el-progress>
+        <!-- element-ui集成element-ui -->
         <!-- 路由中的内容展现在页面上 -->
         <router-view></router-view>
     </div>
--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -1,18 +1,28 @@
 <template>
     <div>
+        <p>************</p>
         <div>{{title}}</div>
-        {{title}}
+        <div class="block">
+            <!-- <span class="demonstration">自定义初始值</span> -->
+            <el-slider v-model="value2"></el-slider>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                title:'登录页面'
-            }
-        }
+export default {
+  data() {
+    return {
+      title: "登录页面",
+        value2: 50,
+    };
+  },
+   methods: {
+      formatTooltip(val) {
+        return val / 100;
+      }
     }
+};
 </script>
 
 <style scoped>
