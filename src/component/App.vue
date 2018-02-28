35d3896d7f5a13231b6dbadd527df5db9002b124
heminhua <15218482830@163.com>
2018-02-28 10:07:12 +0800

    集成axios和normalize.css

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,20 +1,28 @@
 <template>
-    <div>
-        <p>{{val}}</p>
-        <!--测试 element-ui集成element-ui -->
-        <el-progress type="circle" :percentage="100" status="success"></el-progress>
-        <!-- element-ui集成element-ui -->
-        <!-- 路由中的内容展现在页面上 -->
-        <router-view></router-view>
-    </div>
+  <div>
+    <p>{{val}}</p>
+    <!--测试 element-ui集成element-ui -->
+    <el-progress type="circle" :percentage="100" status="success"></el-progress>
+    <!-- element-ui集成element-ui -->
+    <!-- 路由中的内容展现在页面上 -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+// import axios from "axios"; //不是vue 插件，导入之后只能 在当前模块使用
+// import api, { domain } from "../js/api.js";
+
 export default {
   data() {
     return {
       val: "首页"
     };
+  },
+  //生命周期函数，vue会自动调用
+  created() {
+    // axios.get(domain+api.logout.then(()=>console.log('登出')))
+    this.$http.get(this.$api.logout).then(() => console.log("登出"));
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ import App from './component/App.vue'
 
 //引入路由
 import router from './router/vueRouter.js'
+//引入初始化css
+import 'normalize.css'
+
+import axios from 'axios' //不是vue 插件，导入之后只能 在当前模块使用
+import api, { domain } from './js/api.js'
+
+
+
+//配置默认域名，这样请求的时候就不用在url里每次手动添加域名了
+axios.defaults.baseURL = domain;
+//因为我们调用的接口跨域了，默认情况下接口给我们设置cookei无效，为了让它有效，我们必须添加此配置
+axios.defaults.withCredentials = true
+
+//为了使用方便，指导axios和api 添加到Vue原型，将来vue组件就可以直接使用
+Vue.prototype.$http = axios;
+Vue.prototype.$api = api
+
+// axios.get(domain+api.logout.then(()=>console.log('登出')))
+//为了 使用方便，把axios和api添加到vue原型，将来嫺组件就可以直接使用
+
 
 //使用element-ui插件 
 import ElementUI from 'element-ui'
@@ -15,7 +35,7 @@ Vue.use(ElementUI)
 
 //渲染根组件到点位标签上
 new Vue({
-    el:'#app',
+    el: '#app',
     router,//路由注入Vue的实例中
-    render:h=>h(App)
+    render: h => h(App)
 })
